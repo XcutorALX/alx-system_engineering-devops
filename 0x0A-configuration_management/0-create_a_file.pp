2e337eba6a985ca9583ebe81bb0e3f9f373cf39c
Xcutor <Lekanabass50@gmail.com>
2024-04-21 08:03:32 -0500

    task done

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
 # Create a file in /tmp
 file { 'school':
-  path    => '/tmp/school',
   ensure  => file,
+  path    => '/tmp/school',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
