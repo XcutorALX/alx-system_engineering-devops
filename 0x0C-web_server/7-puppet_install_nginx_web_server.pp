eee57dd882dd53e38d032e5227934fcda5e1fe03
Xcutor <Lekanabass50@gmail.com>
2024-04-23 11:15:24 -0500

    task done

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,26 +1,34 @@
-package { 'nginx':
-  ensure   => installed,
-  provider => 'apt',
-}
-
-# Configure Nginx
-class { 'nginx':
-  listen_port  => 80,
-  server_names => [],
-}
+#package { 'nginx':
+#  ensure => 'installed'
+#}
 
 # Create the custom root html
-file { '/var/www/html/index.html':
-  ensure  => file,
-  content => 'Hello World!',
-}
+#file { '/var/www/html/index.html':
+#  ensure  => file,
+#  content => 'Hello World!',
+#}
 
-# Configure a location block for redirection
-nginx::resource::location { 'redirect_me':
-  ensure         => present,
-  vhost          => 'default',
-  location       => '/redirect_me',
-  ensure_values  => [
-    'return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;',
-  ],
+# Create the config file
+file { '/home/unchained/random':
+  ensure => file,
+  content => 'server {
+        listen 80 default_server;
+        listen [::]:80 default_server;
+
+        root /var/www/html;
+
+        index index.html index.htm index.nginx-debian.html;
+
+        server_name _;
+
+        error_page 404 /404.html;
+
+        location /redirect_me {
+                return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+        }
+
+        location / {
+                try_files $uri $uri/ =404;
+        }
+}'
 }
