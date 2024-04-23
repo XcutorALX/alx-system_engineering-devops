2c7042d941f5c3c80a5b8b7f48fb8fe6edb1e95d
Xcutor <Lekanabass50@gmail.com>
2024-04-22 21:58:45 -0500

    task done

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,28 @@
+include nginx
+
+package { 'nginx':
+  ensure   => installed,
+  provider => 'apt',
+}
+
+# Configure Nginx
+class { 'nginx':
+  listen_port  => 80,
+  server_names => [],
+}
+
+# Create the custom root html
+file { '/var/www/html/index.html'
+  ensure  => file,
+  content => 'Hello World!',
+}
+
+# Configure a location block for redirection
+nginx::resource::location { 'redirect_me':
+  ensure         => present,
+  vhost          => 'default',
+  location       => '/redirect_me',
+  ensure_values  => [
+    'return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;',
+  ],
+}
