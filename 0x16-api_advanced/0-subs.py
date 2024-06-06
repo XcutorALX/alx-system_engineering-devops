b7d52da1a52a849b371be315096a6290c077dc72
Xcutor <Lekanabass50@gmail.com>
2024-06-05 19:18:13 -0500

    task done

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-This module contains a 
+This module contains a
 number_of_subscribers function
 """
 
@@ -12,8 +12,9 @@ def number_of_subscribers(subreddit):
     a function that queries the Reddit API
     and returns the number of subscribers
     """
-    url = "https://api.reddit.com/r/{}/about".format(str(subreddit))
-    result = requests.get(url)
+    url = "https://api.reddit.com/r/{}/about/".format(str(subreddit))
+    u_agent = {'User-Agent': '/u/Suspicious-Jelly920'}
+    result = requests.get(url, headers=u_agent)
     if result.status_code == 200:
         return (result.json()['data']['subscribers'])
     return (0)
