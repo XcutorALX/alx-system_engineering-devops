4ede607ca106f7df664a149963a602bfd4e34748
Xcutor <Lekanabass50@gmail.com>
2024-06-05 19:20:54 -0500

    task done

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -12,7 +12,7 @@ def number_of_subscribers(subreddit):
     a function that queries the Reddit API
     and returns the number of subscribers
     """
-    url = "https://api.reddit.com/r/{}/about/".format(str(subreddit))
+    url = "https://api.reddit.com/r/{}/about/".format(subreddit)
     u_agent = {'User-Agent': '/u/Suspicious-Jelly920'}
     result = requests.get(url, headers=u_agent)
     if result.status_code == 200:
